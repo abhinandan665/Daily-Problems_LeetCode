2f8955839aff65946735bc4cd22f08c572358071
abhinandan665 <abhinandanmeena3@gmail.com>
2026-07-31 16:25:29 +0530

    Time: 9 ms (94.22%), Space: 47.9 MB (73.7%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -7,15 +7,9 @@ class Solution {
         Arrays.sort(freq);
         int cnt=0;
         int ope=0;
-        for(int i=freq.length-1;i>=0;i--){
-            cnt++;
-            if(cnt<=8){
-                ope+=freq[i];
-            }else if(cnt<=16){
-                ope+=2*freq[i];
-            }else if(cnt<=24){
-                ope+=3*freq[i];
-            }else ope+=4*freq[i];
+        for(int i=25;i>=0;i--){
+            ope+=freq[i]*(cnt/8+1);
+            cnt++;  
         }
         return ope;
     }
