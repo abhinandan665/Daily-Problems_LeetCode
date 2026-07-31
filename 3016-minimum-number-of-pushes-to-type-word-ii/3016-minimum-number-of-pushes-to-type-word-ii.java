5458193cd81da4cb0d73f34040533610e9e45247
abhinandan665 <abhinandanmeena3@gmail.com>
2026-07-31 16:21:08 +0530

    Time: 10 ms (66.76%), Space: 48 MB (73.7%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,26 +1,22 @@
 class Solution {
     public int minimumPushes(String word) {
-        HashMap<Character,Integer> map=new HashMap<>();
+        int[] freq=new int[26];
         for(char ch:word.toCharArray()){
-            map.put(ch,map.getOrDefault(ch,0)+1);
+            freq[ch-'a']++;
         }
-        List<Integer> list=new ArrayList<>();
-        for(int value:map.values()){
-            list.add(value);
-        }
-        Collections.sort(list);
-        int oprat=0;
+        Arrays.sort(freq);
         int cnt=0;
-        for(int i=list.size()-1;i>=0;i--){
+        int ope=0;
+        for(int i=freq.length-1;i>=0;i--){
             cnt++;
             if(cnt<=8){
-                oprat+=list.get(i);
+                ope+=freq[i];
             }else if(cnt<=16){
-                oprat+=2*list.get(i);
+                ope+=2*freq[i];
             }else if(cnt<=24){
-                oprat+=3*list.get(i);
-            }else oprat+=4*list.get(i);
+                ope+=3*freq[i];
+            }else ope+=4*freq[i];
         }
-        return oprat;
+        return ope;
     }
 }
